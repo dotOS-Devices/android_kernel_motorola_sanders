512ac5998133bbf255c3f80ee4dfc619cb801fc7
ronaxdevil <pratabidya.007@gmail.com>
2019-02-28 15:54:47 +0000

    Cpufreq: Add blu_schedutil cpu governers

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -563,6 +563,9 @@ extern struct cpufreq_governor cpufreq_gov_blu_active;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELECTRON)
 extern struct cpufreq_governor cpufreq_gov_electron;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_electron)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_SCHEDUTIL)
+extern struct cpufreq_governor cpufreq_gov_blu_schedutil;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_schedutil)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
