0a771d08990b2285fe65680c1bd5e16bbc2ffd5b
utsavbalar1231 <utsavbalar1231@gmail.com>
2019-03-23 18:00:37 +0000

    msm: camera: actuator: don't boost if display is off
    Signed-off-by: ronaxdevil <pratabidya.007@gmail.com>

--- drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
+++ drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -13,6 +13,9 @@
 #define pr_fmt(fmt) "%s:%d " fmt, __func__, __LINE__
 
 #include <linux/module.h>
+#include <linux/cpu_input_boost.h>
+#include <linux/devfreq_boost.h>
+#include <linux/state_notifier.h>
 #include "msm_sd.h"
 #include "msm_actuator.h"
 #include "msm_cci.h"
@@ -599,8 +602,10 @@ static int32_t msm_actuator_move_focus(
 
 	CDBG("called, dir %d, num_steps %d\n", dir, num_steps);
 
-	cpu_input_boost_kick_max(150);
-	devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 150);
+	if (!state_suspended) {
+		cpu_input_boost_kick_max(500);
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 500);
+		}
 
 	if (dest_step_pos == a_ctrl->curr_step_pos)
 		return rc;
