3b8793098400b47cd889af628b74952b6399aaf8
ronaxdevil <pratabidya.007@gmail.com>
2019-04-09 12:01:48 +0000

    linux: cpufreq: Add ElectroUtil cpufreq governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -566,6 +566,9 @@ extern struct cpufreq_governor cpufreq_gov_electron;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_SCHEDUTIL)
 extern struct cpufreq_governor cpufreq_gov_blu_schedutil;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_schedutil)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELECTROUTIL)
+extern struct cpufreq_governor cpufreq_gov_electroutil;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_electroutil)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
